368dfdff3ce6d063963357cf7efe3bf7c2e94e6a
Viktoriia Shalaieva <v.shalaieva@ff-group.cz>
2025-12-28 21:08:26 +0200

    Added filtering and charts to dashboard

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
+st.set_page_config(layout="wide")
 
 st.title('Titanic Dashboard')
 
@@ -17,3 +19,29 @@ selected_port = col1.selectbox(options=embarked_port,
                                label='Select  a port')
 selected_gender = col2.selectbox(options=gender,
                                  label='Select  a gender')
+
+# Filter dataframe based on the widget values
+df_plot = df[df['Embarked'] == selected_port]
+df_plot = df_plot[df_plot['Sex'] == selected_gender]
+
+plot = px.histogram(data_frame=df_plot,
+                    template='seaborn',
+                    color='Survived',
+                    title='Distribution of Age',
+                    facet_col='Survived',
+                    x='Age')
+col1.plotly_chart(plot)
+
+# create the pie chart using plotly express
+df_plot_pie = df_plot.loc[:, ['PassengerId', 'Survived']].groupby('Survived').count().reset_index()
+df_plot_pie.rename({'PassengerId': 'Count of passengers'},
+                   axis='columns',
+                   inplace=True)
+
+pie_plot = px.pie(data_frame=df_plot_pie,
+                  template='seaborn',
+                  title='Count of passengers that survived',
+                  values='Count of passengers',
+                  names='Survived')
+col2.plotly_chart(pie_plot)
+
