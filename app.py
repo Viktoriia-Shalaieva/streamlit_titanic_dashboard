a6c7f96dff32e63927361c1a58292ce02083b2eb
Viktoriia Shalaieva <v.shalaieva@ff-group.cz>
2025-12-28 20:29:31 +0200

    Changes made to layout

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 import streamlit as st
+import pandas as pd
 
 
 st.title('Titanic Dashboard')
+
+df = pd.read_csv('titani_data.csv')
+
+df['Embarked'] = df['Embarked'].fillna('Unknown')
+
+# get unique values fpr embarked port
+embarked_port = list(df['Embarked'].unique())
+gender = list(df['Sex'].unique())
+
+col1, col2 = st.columns([1,1])
+selected_port = col1.selectbox(options=embarked_port,
+                               label='Select  a port')
+selected_gender = col2.selectbox(options=gender,
+                                 label='Select  a gender')
